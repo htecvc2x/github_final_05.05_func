cebfacbe5312c498cbd26914e50eb21140dddda6
Maxim Antonuk <maxim.antonuk@gmail.com>
2020-05-06 07:52:36 +0300

    spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,43 +3,39 @@ import PropTypes from 'prop-types';
 
 import './Spinner.css'
 
-export default class Spinner extends Component {
-    render() {
-        const {loading} = this.props;
-
-        let styleDisplay = 'none';
-        if (loading) {
-            styleDisplay = 'block';
-        }
-
-        return (
-            <div
-                style={{
-                    position: "fixed",
-                    top: 0,
-                    left: 0,
-                    backgroundColor: "rgba(0,0,0,.8)",
-                    height: "100vh",
-                    width: "100%",
-                    display: styleDisplay
-                }}
-            >
-                <div
-                    style={{ position: "absolute", top: "50%", left: "50%" }}
-                    alt="Spinner loader"
-                    className="lds-roller">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-            </div>
-        )
+const Spinner = ({loading}) => {
+    let styleDisplay = 'none';
+    if (loading) {
+        styleDisplay = 'block';
     }
+
+    return (
+        <div
+            style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                backgroundColor: "rgba(0,0,0,.8)",
+                height: "100vh",
+                width: "100%",
+                display: styleDisplay
+            }}
+        >
+            <div
+                style={{ position: "absolute", top: "50%", left: "50%" }}
+                alt="Spinner loader"
+                className="lds-roller">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
 }
 
 Spinner.defaultProps = {
@@ -50,3 +46,4 @@ Spinner.propTypes = {
       loading : PropTypes.bool
 };
 
+export default  Spinner;
